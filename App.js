a1339565186fb0c11c745e0ab9eb7832546b1237
Adit <aditnegi1@gmail.com>
2020-07-23 00:04:09 +0530

    frist

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  Image,
+  SafeAreaView,
+  Alert,
+  Button,
+  Platform,
+} from "react-native";
 
 export default function App() {
+  const handlePress = () => console.log("Text Pressed");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Hello React native
+      </Text>
+      <TouchableHighlight>
+        <Image
+          blurRadius={2}
+          fadeDuration={1000}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableHighlight>
+      <Button
+        title="click me"
+        onPress={() => Alert.alert("The title", "bleh message")}
+      />
       <StatusBar style="auto" />
-    </View>
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          flex: 0.5,
+        }}
+      ></View>
+    </SafeAreaView>
   );
 }
+
+const containerStyle = { backgroundColor: "orange" };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 20,
   },
 });
